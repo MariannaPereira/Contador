0510aea91bfc218ebb471b8ee2868f2e08de1b3d
MariannaPereira <maricerqueira6567@gmail.com>
2024-02-05 19:00:04 -0400

    aula vnw

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contador from "./Contador/Contador.jsx"
+function App (){
+  return(
+<>
+<Contador/>
+<h1>Olá T3!</h1>
+<h2>Segundoooou!</h2>
+</>
+  )
+}
+
+export default App
